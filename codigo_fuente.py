793c2e389cf3b9967ddad5077fe1433a4cfe8682
María José Sáenz <m.saenzr@uniandes.edu.co>
2023-02-07 00:32:25 -0500

    funcion1

--- codigo_fuente.py
+++ codigo_fuente.py
@@ -1,1 +1,9 @@
-#Prueba subida plataforma.
+
+
+def posicion_N(N):
+    """Función encargada de retornar la posición final (r_final)
+    de acuerdo al número de pasos (N) que entra como parámetro. Además,
+    también entra como parámetro la posición inicial en la que se encuentra
+    la partícula (r_inicial)."""
+    r_final = 0
+    return r_final
